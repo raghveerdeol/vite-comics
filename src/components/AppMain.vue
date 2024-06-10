c89039bf376e48fca4625463fd1d4d633e46217b
raghveerdeol <deolraghveer5@gmail.com>
2024-06-10 20:45:28 +0200

    aggiunto contenuto padding e background-color

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,10 +8,16 @@ data() {
 
 <template>
     <main>
-        main
+        <h1>-->content goes here<--</h1>
     </main>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+main{
+    background-color: $bg-main;
+    color: white;
+    @include padMain();
+}
 </style>

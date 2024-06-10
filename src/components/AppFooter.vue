8a2bf112954b193d19feb890ab69f8a214c44ebf
raghveerdeol <deolraghveer5@gmail.com>
2024-06-10 21:02:57 +0200

    creati oggetti dei dati nel footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,17 +1,155 @@
 <script>
 export default {
 data() {
-
+    return {
+        dcComixLinks: [
+            {
+                id: 1,
+                title: "characters",
+                url: "#",
+            },
+            {
+                id: 2,
+                title: "comics",
+                url: "#",
+            },
+            {
+                id: 3,
+                title: "movies",
+                url: "#",
+            },
+            {
+                id: 4,
+                title: "tv",
+                url: "#",
+            },
+            {
+                id: 5,
+                title: "games",
+                url: "#",
+            },
+            {
+                id: 6,
+                title: "videos",
+                url: "#",
+            },
+            {
+                id: 7,
+                title: "news",
+                url: "#",
+            },
+        ],
+        shopLInks: [
+            {
+                id: 1,
+                title: "Shop DC",
+                url: "#"
+            },
+            {
+                id: 2,
+                title: "Shop DC Collectibles",
+                url: "#"
+            },
+        ],
+        dcLinks: [
+            {
+                id: 1,
+                title: "Terms of Use",
+                url: "#",
+            },
+            {
+                id: 2,
+                title: "Privacy policy(New)",
+                url: "#",
+            },
+            {
+                id: 3,
+                title: "Ad Choices",
+                url: "#",
+            },
+            {
+                id: 4,
+                title: "Advertising",
+                url: "#",
+            },
+            {
+                id: 5,
+                title: "Jobs",
+                url: "#",
+            },
+            {
+                id: 6,
+                title: "Subscriptions",
+                url: "#",
+            },
+            {
+                id: 7,
+                title: "Talent Workshops",
+                url: "#",
+            },
+            {
+                id: 8,
+                title: "CPSC Certificates",
+                url: "#",
+            },
+            {
+                id: 9,
+                title: "Ratings",
+                url: "#",
+            },
+            {
+                id: 10,
+                title: "Shop Help",
+                url: "#",
+            },
+            {
+                id: 11,
+                title: "Contact Us",
+                url: "#",
+            },
+            
+        ],
+        sitesLinks: [
+            {
+                id: 1,
+                title: "DC",
+                url: "#",
+            },
+            {
+                id: 2,
+                title: "MAD Magazine",
+                url: "#",
+            },
+            {
+                id: 3,
+                title: "DC Kids",
+                url: "#",
+            },
+            {
+                id: 4,
+                title: "DC Universe",
+                url: "#",
+            },
+            {
+                id: 5,
+                title: "DC Powe Visa",
+                url: "#",
+            },
+            
+        ],
+        
+    },
 }
 }
 </script>
 
 <template>
     <footer>
-        footer
+        
     </footer>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
 </style>

9b9013841a33f3d51bfd37e9cc91c9a4481c6c18
raghveerdeol <deolraghveer5@gmail.com>
2024-06-11 14:51:43 +0200

    aggiunto current series con position relative

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -11,10 +11,13 @@ return {
 <template>
     <div class="jumbotron">
         <img src="../assets/img/jumbotron.jpg" alt="teen titans">
+        <span>current series</span>
     </div>
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
     .jumbotron{
         max-width: 100%;
             img{    
@@ -23,5 +26,14 @@ return {
                 object-fit: cover;
                 object-position: top;
             }
+            span{
+                position: relative;
+                bottom: 15px;
+                left: 140px;
+                background-color: $bg-logo;
+                padding: .7rem 2rem;
+                text-transform: uppercase;
+                font-weight: 900;
+                }
     }
 </style>
